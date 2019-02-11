a631c26aa0a1386a81bb2dcdf35d6fd9c9021b31
Lia Hardy <hardywillia@outlook.com>
2019-02-11 14:15:16 -0500

    added include files and commit format to main program

--- omnibot.cpp
+++ omnibot.cpp
@@ -1,1 +1,22 @@
-//Start of main program for the robot
+//Main Omnibot Program
+
+/* Last edit logs:
+
+    -Willia 02/11/19: Added file and commit format
+
+
+
+*/
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <stdio.h>
+
+
+int main(){
+
+
+
+
+    return 0;
+}
