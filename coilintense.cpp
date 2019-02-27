7a16bebb100c67b01fcd7ffc8635a92c577a432f
Lia Hardy <hardywillia@outlook.com>
2019-02-26 21:22:17 -0500

    added magnitude function for coil intensity

--- coilintense.cpp
+++ coilintense.cpp
@@ -3,6 +3,7 @@
 /* Last edit logs:
 
     -Willia 02/11/19: Added base pseudocode from diagram
+    -Willia 02/26/2019: Added function for computing magnitude
 
 
 
@@ -14,34 +15,63 @@
 #include <cmath>
 #include <thread>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 #define PI 3.14159265
 
 class OmnibotCoilIntensity{
 
-    //1ST FUNCTION
+    double x = 0;
+    double y = 0;
+    double theta[100];
+    double phi[100];
+    int radius = 17;
+    double coilAddress[300];
+    double intendvec;
+
+    //1ST FUNCTION ?? need clarification
     //Receive the angle of the coil furthest (theta) and the coil closest (phi) from the intended vector (output from control algorithm)
 
     //2ND FUNCTION
     //Intensify the furthest coil to 100%
-        //Compute the sin(|theta|)
-        //Multiply by the magnitude output of that coil at a max current of 30Amps
-
-    //3RD FUNCTION
     //Calculate the force of the closest coil
-        //Use (sin(|theta|) * (max forceof furthest coil))/(sin(|phi|))
-
-    //4TH FUNCTION
     //Calculate the current vector magnitude
-        //Use cos(|theta|) * max(force of furthest coil) + cos(|phi|) * (force of closest coil)
-
-    //5TH FUNCTION
-    //Multiply the magnitudes of force at both coils by (intended vector magnitude) / (current vector magnitude)
+    //Multiply the magnitudes of force at both coils
         //Get the magnitudes for each coil
-
-
     //Output the magnitudes (determines coil intensity for switching)
 
+    double magOutput(){
+
+        double farcoil;
+        double farcoilforce;
+        double closecoilforce;
+        double vecmag;
+        double farcoilmag;
+        double closecoilmag;
+        int intensity;
+        int num = 0;
+
+        farcoil = sin(theta[num]) * 30;
+        closecoilforce = (sin(abs(theta[num]) * 30))/(sin(abs(phi[num])));
+        vecmag = cos(abs(theta[num])) * max(farcoilforce, 100.0) + cos(abs(phi[num])) * (closecoilforce);
+
+        num++;
+
+        cout << "What is the intended vector: " << endl;
+        cin >> intendvec;
+
+        farcoilforce *= intendvec / vecmag;
+        closecoilforce *= intendvec / vecmag;
+
+        farcoilmag = farcoilforce;
+        closecoilmag = closecoilforce;
+
+        //Used to check the magnitude for MIDTERM purposes
+        cout << farcoilmag;
+        cout << closecoilmag;
+
+        return farcoilmag, closecoilmag;
+    }
 
 }
