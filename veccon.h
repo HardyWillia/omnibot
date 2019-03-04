2bbce98d1ca7fde5e13e617e291d3d9866a84595
Lia Hardy <hardywillia@outlook.com>
2019-03-04 11:24:15 -0500

    Moved code to another file for efficiency

--- veccon.h
+++ veccon.h
@@ -20,22 +20,9 @@
 #include <thread>
 #include <fstream>
 #include <algorithm>
-#include <vector>
-#include <list>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <iterator>
 
 using namespace std;
 #define PI 3.14159265
-
-// Point in the xy-plane
-struct PointType
-{
-    double x;
-    double y;
-};
 
 double theta[100];
 double phi[100];
@@ -120,113 +107,6 @@ double pickMapping()
         cout << "Unable to open file";
 
     return angle, x, y, mapping[2];
-}
-
-//1ST FUNCTION
-//Find distance of every nearby coil within radial distance
-//Loop through addresses and add to a data structure
-//Determine coils that are within 1 to 3.5cm
-
-//Store all coils within that distance in a data structure
-//Check if it is a deadzone
-//If it is a deadzone, stall the program for () secs. Do not power on any coils
-
-//Use convex hull algorithm to find x and y closest and furthest from the torso
-
-// Sort criterion: points are sorted with respect to their x-coordinate.
-//                 If two points have the same x-coordinate then we compare
-//                 their y-coordinates
-bool sortPoints(const PointType &lhs, const PointType &rhs)
-{
-    return (lhs.x < rhs.x) || (lhs.x == rhs.x && lhs.y < rhs.y);
-}
-
-// Check if three points make a right turn using cross product
-bool right_turn(const PointType &P1, const PointType &P2, const PointType &P3)
-{
-    return ((P3.x - P1.x) * (P2.y - P1.y) - (P3.y - P1.y) * (P2.x - P1.x)) > 0;
-}
-
-double findDistance()
-{
-
-    ifstream myfile;
-    //PointType *points;
-    vector<PointType> lowerCH;
-    vector<PointType> upperCH;
-
-    // reading data from file
-    myfile.open("coilcoordinates.txt");
-
-    if (myfile.is_open())
-    {
-        //myfile >> n_points;
-        //points = new PointType[999];
-        vector<PointType> points;
-        string line;
-        int row = 0;
-        while (getline(myfile, line))
-        {
-            std::stringstream ss(line);
-            std::string token;
-            int col = 1;
-
-            PointType point;
-            while (std::getline(ss, token, ' '))
-            {
-                if (col == 1)
-                {
-                    point.x = stod(token);
-                }
-                else
-                {
-                    point.y = stod(token);
-                }
-                col++;
-            }
-            points.push_back(point);
-            row++;
-        }
-        // for (int i = 0; i < n_points; i++)
-        //     myfile >> points[i].x >> points[i].y;
-
-        myfile.close();
-
-        //Sorting points
-        //sort(points, points + n_points, sortPoints);
-        //cout << "Sorted Points\n";
-        //for (int i = 0; i != n_points; ++i)
-           // cout << "(" << points[i].x << " , " << points[i].y << ")" << endl;
-
-
-        //Computing upper convex hull
-        upperCH.push_back(points[0]);
-        upperCH.push_back(points[1]);
-
-        for (int i = 2; i < points.size(); i++)
-        {
-            while (upperCH.size() > 1 and (!right_turn(upperCH[upperCH.size() - 2], upperCH[upperCH.size() - 1], points[i])))
-                upperCH.pop_back();
-            upperCH.push_back(points[i]);
-            cout << "Sorted points: " << sortPoints(points[i-2], points[i-1]) << endl; 
-        }
-        cout << "Furthest coil: " << endl;
-        cout << "(" << upperCH[upperCH.size()-1].x << " , " << upperCH[upperCH.size()].y << ")" << endl;
-
-        //Computing lower convex hull
-        lowerCH.push_back(points[points.size() - 1]);
-        lowerCH.push_back(points[points.size() - 2]);
-
-        for (int i = 2; i < points.size(); i++)
-        {
-            while (lowerCH.size() > 1 and (!right_turn(lowerCH[lowerCH.size() - 2], lowerCH[lowerCH.size() - 1], points[points.size() - i - 1])))
-                lowerCH.pop_back();
-            lowerCH.push_back(points[points.size() - i - 1]);
-        }
-        cout << "Closest coil: " << endl;
-        for (int i = 0; i < lowerCH.size(); i++)
-            cout << "(" << lowerCH[i].x << " , " << lowerCH[i].y << ")" << endl;
-    }
 }
 
 #endif /* VECCON_H */
