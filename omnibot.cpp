8c181f39b444d472ced1f4b96527a97d512e389b
Lia Hardy <hardywillia@outlook.com>
2019-03-06 14:08:07 -0500

    fixing for loop

--- omnibot.cpp
+++ omnibot.cpp
@@ -37,7 +37,7 @@ int main(){
     PointType points[] = { { 0, 3 }, { 1, 1 }, { 2, 2 }, { 3, 2 }, { 0, 0 },
             { 1, 2 }, { 3, 1 }, { 3, 3 } };
     for (int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
+        for(int j = 0; j < i; j++){
             cout << "Coil " << coilnum << " is coordinates: " << "(" << points[i].x << ", " << points[j].y1 << ")" << endl;
             coilnum++;
         }
