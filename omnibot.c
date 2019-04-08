807fa5ce143b391e57012cf3322e7439fb71a981
Lia Hardy <hardywillia@outlook.com>
2019-04-08 17:36:30 -0400

    Update and rename omnibot.cpp to omnibot.c

--- omnibot.c
+++ omnibot.c
@@ -1,30 +1,26 @@
 //Main Omnibot Program
 
 /* Last edit logs:
-
     -Willia 02/11/19: Added file and commit format
     -Willia 02/26/19: Started to create test cases for program
     -Willia 03/04/19: Removed need for file and changed to array
     -Willia 03/06/19: Need to add all the possible coordinates for coils after the midterm
     -Willia 04/03/19: Finished initial coil identification, there is a bug in the array
-
-
 */
-#include <iostream>
-#include <string>
-#include <cstdlib>
+//#include <iostream>
+#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 #include "coilintense.h"
 
-using namespace std;
-
-
 
 int main(){
 
-
+    while(1){
     magOutput();
+    }
+   
 
     return 0;
 }
