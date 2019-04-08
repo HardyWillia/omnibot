4b5725f53365b1652c094e58dace9b7a910a306a
Lia Hardy <hardywillia@outlook.com>
2019-04-08 11:01:28 -0400

    added more conditions based on coil positions

--- coilintense.h
+++ coilintense.h
@@ -8,6 +8,7 @@
     -Willia 03/04/19: Debugging the code to determine why the values are printing incorrectly
     -Willia 04/01/19: Added coilintensetest as a test program for further improvements
     -Willia 04/03/19: Finished initial coil identification, there is a bug in the array
+    -Willia 04/08/19: Added more conditions based on coil positions
 
 */
 
@@ -37,7 +38,6 @@ double magOutput(int n)
     double theta[100];
     double phi[100];
     double intendvec;
-    double farcoil;
     double farcoilforce;
     double closecoilforce;
     double vecmag;
@@ -90,25 +90,38 @@ double magOutput(int n)
                 ++num;
 
                 //Sort through the coordinates to find the closest to a given point
-                int currentposx;
-                int currentposy;
+                int currentposx = int (x);
+                int currentposy = int (y);
 
                 for (int i = 1; i < n; ++i)
                 {
 
-                    cout << "What is your current position: ";
-                    cin >> currentposx >> currentposy;
+                    cout << "Your current position: (" << currentposx << ", " << currentposy << ")" << endl;
 
                     if (currentposx >= 6)
                     {
                         currentposx = 1;
+
+                        if (angle >= 0.0 && angle < 40.0)
+                        {
+                            currentposx += 1;
+                        }
+                        else if (angle > 40.0 && angle <= 90.0)
+                        {
+                            currentposy += 1;
+                        }
+                        else if (angle > 90 && angle <= 180)
+                        {
+                            currentposx = abs(7 - currentposx);
+                            currentposy += 2;
+                        }
                     }
                     else
                     {
                         currentposx += 1;
                     }
 
-                    farcoil = sin(theta[num]) * 30;
+                    farcoilforce = sin(theta[num]) * 30;
                     closecoilforce = (sin(abs(theta[num]) * 30)) / (sin(abs(phi[num])));
 
                     /*
@@ -119,7 +132,7 @@ double magOutput(int n)
                     100% = 304W
                     50% = 152W
                 */
-                    vecmag = cos(abs(theta[num])) * max(farcoil, 100.0) + cos(abs(phi[num])) * (closecoilforce);
+                    vecmag = cos(abs(theta[num])) * max(farcoilforce, 100.0) + cos(abs(phi[num])) * (closecoilforce);
 
                     int intendedmag;
                     cout << "What is the intended vector magnitude: ";
@@ -172,22 +185,36 @@ double magOutput(int n)
                 }
 
                 //Sort through the coordinates to find the closest to a given point
-                int currentposx;
-                int currentposy;
+                int currentposx = int (x);
+                int currentposy = int (y);
 
                 for (int i = 1; i < n; ++i)
                 {
 
-                    cout << "What is your current position: ";
-                    cin >> currentposx >> currentposy;
+                    cout << "Your current position: (" << currentposx << ", " << currentposy << ")" << endl;
 
                     if(currentposx >= 6){
                             currentposx = 1;
-                    } else {
+
+                            if (angle >= 0.0 && angle < 40.0)
+                            {
+                                currentposx += 1;
+                            }
+                            else if (angle > 40.0 && angle <= 90.0)
+                            {
+                                currentposy += 1;
+                            }
+                            else if (angle > 90 && angle <= 180)
+                            {
+                                currentposx = abs(7 - currentposx);
+                                currentposy += 2;
+                            }
+                    }
+                    else {
                         currentposx += 1;
                     }
 
-                    farcoil = sin(theta[num]) * 30;
+                    farcoilforce = sin(theta[num]) * 30;
                     closecoilforce = (sin(abs(theta[num]) * 30)) / (sin(abs(phi[num])));
 
                     /*
@@ -198,7 +225,7 @@ double magOutput(int n)
                     100% = 304W
                     50% = 152W
                 */
-                    vecmag = cos(abs(theta[num])) * max(farcoil, 100.0) + cos(abs(phi[num])) * (closecoilforce);
+                    vecmag = cos(abs(theta[num])) * max(farcoilforce, 100.0) + cos(abs(phi[num])) * (closecoilforce);
 
                     int intendedmag;
                     cout << "What is the intended vector magnitude: ";
