40b5ffee9c991194aa6a735441c3246d1f1b273a
Lia Hardy <hardywillia@outlook.com>
2019-04-08 20:35:37 -0400

    trying to find the error

--- coilintense.h
+++ coilintense.h
@@ -43,7 +43,7 @@ double angle, result, x, y;
 
 
 // Points in the Cartesian plane
-void currentposition(int currentposx, int currentposy, double x, double y){
+void currentposition(int currentposx, int currentposy){
 
     int n = 2;
     int farposx = abs(6 - currentposx);
@@ -76,8 +76,8 @@ void currentposition(int currentposx, int currentposy, double x, double y){
             currentposx += 1;
         }
 
-        farcoilforce = sin(x) * 30;
-        closecoilforce = (sin(abs(x) * 30)) / (sin(abs(y)));
+        farcoilforce = sin(theta[num]) * 30;
+        closecoilforce = (sin(abs(theta[num]) * 30)) / (sin(abs(phi[num])));
 
         /*
                     Max current 30A
@@ -88,12 +88,11 @@ void currentposition(int currentposx, int currentposy, double x, double y){
                     50% = 152W
                 */
 
-        vecmag = cos(abs(x)) * fmax(farcoilforce, 100.0) + cos(abs(y)) * (closecoilforce);
-        
+        vecmag = cos(abs(theta[num])) * max(farcoilforce, 100.0) + cos(abs(phi[num])) * (closecoilforce);
 
         int intendedmag;
         printf ("What is the intended vector magnitude: ");
-        scanf("%d", intendedmag);
+        scanf("%d", &intendedmag);
 
         //farcoilforce *= intendedmag/vecmag;
         //farcoilmag = abs(farcoilforce) * 100;
@@ -211,7 +210,7 @@ double magOutput()
         int a = x;
         int b = y;
         printf("Your current position: (%d , %d) \n", a, b);
-        currentposition(a, b, x, y);
+        currentposition(a, b);
         ++num;
         }
     fclose(fp);
