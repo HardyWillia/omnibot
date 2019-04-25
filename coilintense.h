91f077a8408b803c60bd2b0693fe95c17075cd59
Lia Hardy <hardywillia@outlook.com>
2019-04-24 22:00:52 -0400

    Update coilintense.h

--- coilintense.h
+++ coilintense.h
@@ -18,75 +18,78 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-
-//#include "adc.h"
+#include <time.h>
+#include "adc.h"
 
 #define PI 3.14159265
 
-/*ISR
+//ISR
 #define PERIOD_10ms 0x01
 
-void Timer_ISR(void)
+//Coil Array
+int coilcoords[42][3] =
+   { {0, 0, 0},
+    {0, 1, 1},
+    {0, 2, 2},
+    {0, 3, 3},
+    {0, 4, 4},
+    {0, 5, 5},
+    {1, 0, 6},
+    {1, 1, 7},
+    {1, 2, 8},
+    {1, 3, 9},
+    {1, 4, 10},
+    {1, 5, 11},
+    {2, 0, 12},
+    {2, 1, 13},
+    {2, 2, 14},
+    {2, 3, 15},
+    {2, 4, 16},
+    {2, 5, 17},
+    {3, 0, 18},
+    {3, 1, 19},
+    {3, 2, 20},
+    {3, 3, 21},
+    {3, 4, 22},
+    {3, 5, 23},
+    {4, 0, 24},
+    {4, 1, 25},
+    {4, 2, 26},
+    {4, 3, 27},
+    {4, 4, 28},
+    {4, 5, 29},
+    {5, 0, 30},
+    {5, 1, 31},
+    {5, 2, 32},
+    {5, 3, 33},
+    {5, 4, 34},
+    {5, 5, 35},
+    {6, 0, 36},
+    {6, 1, 37},
+    {6, 2, 38},
+    {6, 3, 39},
+    {6, 4, 40},
+    {6, 5, 41}
+};
+
+void matchCoil(int val1, int val2)
 {
-    static char State = PERIOD_10ms;
 
-    switch (State)
+  int i, j, thirdParam;
+  for (i = 0; i < 41; ++i)
+  {
+    for (j = 0; j < 2; ++j)
     {
-    case PERIOD_10ms:
-    {
-
-        // Timer Stop;
-        T1CON = 0x8030; //setting Timer1
-        TMR1 = 0;
-
-        // ADC sampling -- get from Aladdin's code
-        //Average the signals
-        
-        int adcnum = 10;
-        int sum1 = 0;
-        int sum2 = 0;
-        int sum3 = 0;
-        int finalsum = 0; 
-        int avg;
-
-        
-            for(int i = 1; i <= adcnum; i++){
-
-                //pin 22 gets toggled
-                ADC_ChannelEnable(SENSOR1);
-                int adc1 = ADC_ReadData(SENSOR1); 
-
-                //pin 23 gets toggled
-                ADC_ChannelEnable(SENSOR2);
-                int adc2 = ADC_ReadData(SENSOR2); 
-
-                //pin 24 get toggled
-                ADC_ChannelEnable(SENSOR3);
-                int adc3 = ADC_ReadData(SENSOR3); 
-
-                    //Get sum of all readings per sensor
-                    sum1 += adc1;
-                    sum2 += adc2;
-                    sum3 += adc3;
-                    finalsum = sum1 + sum2 + sum3;
-            }
-
-        avg = finalsum / adcnum;
-        printf("Average: %d\n", avg);
-
-        // Timer Start;
-        TMR1 = 1;
-        break;
+       if(val1 == coilcoords[i][0] && val2 == coilcoords[i][1]){
+		      thirdParam = coilcoords[i][2];       
+ 		      printf("Coil(%d , %d) = %d", val1, val2, thirdParam);
+              ms_delay(5000);
+              printf( "\f" ); 
+		      break;
+	      }
     }
-
-    default:
-    {
-        msdelay(10);
-        break;
-    }
-    }
+  }
 }
-*/
 
 //Global variables
 double theta;
@@ -97,10 +100,10 @@ double closecoilforce;
 double vecmag;
 double farcoilmag;
 double closecoilmag;
+int intensity[2];
 int radius = 17;
 
 double angle, result, x, y;
-int closex, closey, farposx, farposy;
 
 //Intensify the furthest coil to 100%, always (until a better method is found)
 //Calculate the force of the closest coil
@@ -157,7 +160,7 @@ ms_delay(5000);
 
     else
     {
-        //Conditons for Mapping 1 (grid)
+        //Conditions for Mapping 1 (grid)
         x = theta;
         y = phi;
         mapping[2] = mapping1[2];
@@ -192,6 +195,7 @@ ms_delay(5000);
     }
     int currentposx = x;
     int currentposy = y;
+    int closex, closey;
     //printf("Your current position: (%d , %d) \n", currentposx, currentposy);
     /*
         ms_delay(5000);
@@ -233,8 +237,8 @@ ms_delay(5000);
         }
     }
 
-    farposx = abs(6 - currentposx);
-    farposy = abs(5 - currentposy);
+    int farposx = abs(6 - currentposx);
+    int farposy = abs(5 - currentposy);
     double val = 30.0;
     farcoilforce = sin(theta) * val;
     closecoilforce = (sin(fabs(theta) * val)) / (sin(fabs(phi)));
@@ -267,6 +271,7 @@ ms_delay(5000);
         closex = closex;
         closey = closey;
         closecoilmag = 100.0;
+        matchCoil(closex, closey);
     }
     else if (closecoilmag == 0.0)
     {
@@ -277,6 +282,7 @@ ms_delay(5000);
         farposx = farposx;
         farposy = farposy;
         farcoilmag = 100.0;
+        matchCoil(farposx, farposy);
     }
     else
     {
@@ -291,13 +297,107 @@ ms_delay(5000);
         farcoilmag = 100.0;
         farposx = farposx;
         farposy = farposy;
+        matchCoil(farposx, farposy);
         
         closecoilmag = closecoilmag;
         closex = closex;
         closey = closey;
+        matchCoil(closex, closey);
     }
     
     return closex, closey, closecoilmag, farposx, farposy, farcoilmag;
 }
+
+void Timer_ISR(void)
+{
+    static char State = PERIOD_10ms;
+    switch (State)
+    {
+    case PERIOD_10ms:
+    {
+        // Timer Stop;
+        T1CON = 0x8030; //setting Timer1
+        TMR1 = 0;
+        // ADC sampling -- get from Aladdin's code
+        //Average the signals
+        
+        int adcnum = 10;
+        int sum1 = 0;
+        int sum2 = 0;
+        int sum3 = 0;
+        int finalsum = 0; 
+        int avg;
+        
+        //Dan's function
+        
+        
+            //Lia's Function
+                //Test speed of pic processing magOutput program
+        clock_t start = clock();
+         //magOutput with read in from control algorithm
+          //Equations from control algorithm when integration is complete
+                /*
+                torquetheta = sin(PI/2 ? thetat) * 0.5003 + (0.127 * ((PI /2 ? thetat) -1) *  thetaprimet + 0.9738 * (PI/2  - thetat);
+                torquephi  = (0.127 * ((? /2 ? thetat) -1) * phiprimet 	
+              */
+
+            magOutput(0, 4.18);
+            magOutput(0.84, 5.23);
+            magOutput(1.7, 4.71);
+            magOutput(0.77, 3.1);
+            magOutput(2.093, 0);
+            magOutput(0.784, 5.3);
+            magOutput(1.57, 4.71);
+            magOutput(0, 3.14);
+            magOutput(2.03, 0);
+            magOutput(2.55, 2.093);
+            magOutput(0.51, 4.8);
+            magOutput(2.3, 2.3);
+            magOutput(0.74, 5);
+        double dur = (clock() - start);
+        printf("test done");
+          ms_delay(5000);
+     printf( "\f" ); 
+      ms_delay(5000);
+        printf("Dur: %d ",dur);
+        ms_delay(5000);
+        printf( "\f" ); 
+        
+                //Alex's Function
+        
+        int i;
+        //Get values from ADC
+            for(i = 1; i <= adcnum; i++){
+                //pin 22 gets toggled
+                ADC_ChannelEnable(SENSOR1);
+                int adc1 = ADC_ReadData(SENSOR1); 
+                //pin 23 gets toggled
+                ADC_ChannelEnable(SENSOR2);
+                int adc2 = ADC_ReadData(SENSOR2); 
+                //pin 24 get toggled
+                ADC_ChannelEnable(SENSOR3);
+                int adc3 = ADC_ReadData(SENSOR3); 
+                    //Get sum of all readings per sensor
+                    sum1 += adc1;
+                    sum2 += adc2;
+                    sum3 += adc3;
+                    finalsum = sum1 + sum2 + sum3;
+            }
+        avg = finalsum / adcnum;
+        printf("Average: %d\n", avg);
+         ms_delay(5000);
+     printf( "\f" ); 
+        // Timer Start;
+        TMR1 = 1;
+        break;
+    }
+    default:
+    {
+        ms_delay(10);
+        break;
+    }
+    }
+}
+
 
 #endif /* COILINTENSE_H */
